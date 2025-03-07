5179abf2d03069efe67e605708e58cff700136f0
Thiago Marques <127054298+ThiagoMSM@users.noreply.github.com>
2025-03-07 10:13:26 -0300

    Update icmsFatecSP.py

--- icmsFatecSP.py
+++ icmsFatecSP.py
@@ -3,6 +3,10 @@ def Main():
 		pcunit = float(input("Informe o valor unitário: "))
 		qtde = int(input("Informe a qtde: "))
 		total = pcunit * qtde
-		print(total)
+		aliq = 0.18
+		vsi = total / (aliq + 1)
+		imposto = total - vsi
+		print(f'Total: R$ {total:.2f}')
+		print(f'Imposto: R$ {imposto:.2f}')
 	
 Main()
