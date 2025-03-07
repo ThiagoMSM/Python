3321847ad98585ebbd6ad647062d9d1d325db7ff
Thiago Marques <127054298+ThiagoMSM@users.noreply.github.com>
2025-03-07 10:22:19 -0300

    Update icmsFatecSP.py, + consistencia

--- icmsFatecSP.py
+++ icmsFatecSP.py
@@ -1,12 +1,15 @@
 def Main():
 	while True:
-		pcunit = float(input("Informe o valor unitário: "))
-		qtde = int(input("Informe a qtde: "))
-		total = pcunit * qtde
-		aliq = 0.18
-		vsi = total / (aliq + 1)
-		imposto = total - vsi
-		print(f'Total: R$ {total:.2f}')
-		print(f'Imposto: R$ {imposto:.2f}')
-	
+		try:
+			pcunit = float(input("Informe o valor unitário: "))
+			qtde = int(input("Informe a qtde: "))
+			total = pcunit * qtde
+			aliq = 0.18
+			vsi = total / (aliq + 1)
+			imposto = total - vsi
+			print(f'Total: R$ {total:.2f}')
+			print(f'Imposto: R$ {imposto:.2f}')
+			print(f'VSI: R$ {vsi:.2f}')
+		except:
+			print("deu ruim")
 Main()
